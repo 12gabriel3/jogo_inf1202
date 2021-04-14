072c1c4593ca851a1f844d3f3ca2adf305438e29
kipper <kipperni10@gmail.com>
2021-04-14 16:27:38 -0300

    codigo comentado

--- src/geometry.c
+++ src/geometry.c
@@ -1,32 +1,37 @@
 #include <geometry.h>
 #include <math.h>
 
-
-COORD dist_from_to(COORD from, COORD to){
+COORD dist_from_to(COORD from, COORD to)
+{
     COORD dist;
     dist.x = to.x - from.x;
     dist.y = to.y - from.y;
     return dist;
 }
 
-void add_to_vector(COORD *dst, float x, float y){
+void add_to_vector(COORD *dst, float x, float y)
+{
     dst->x += x;
     dst->y += y;
 }
 
-float module(COORD v){
+float module(COORD v)
+{
     return sqrt(v.x * v.x + v.y * v.y);
 }
 
-void normalize(COORD *v){
+void normalize(COORD *v)
+{
     float m = module(*v);
-    if(m){
+    if(m)
+    {
         v->x /= m;
         v->y /= m;
     }
 }
 
-COORD input_to_vector(KEYBOARD_STATE input){
+COORD input_to_vector(KEYBOARD_STATE input)
+{
     COORD output_vector;
     output_vector.x = 0;
     output_vector.y = 0;
@@ -38,26 +43,34 @@ COORD input_to_vector(KEYBOARD_STATE input){
     return output_vector;
 }
 
-void add_vector(COORD *pos, COORD delta, float speed){
-    if(module(delta) > 0.5){
+void add_vector(COORD *pos, COORD delta, float speed)
+{
+    if(module(delta) > 0.5)
+    {
         pos->x += delta.x * speed;
         pos->y += delta.y * speed;
     }
 }
 
 
-COORD direction_from_to(COORD from, COORD to){
+COORD direction_from_to(COORD from, COORD to)
+{
     COORD dist = dist_from_to(from, to);
-    if(module(dist) > 1){
+    if(module(dist) > 1)
+    {
         normalize(&dist);
-    } else {
+    }
+    else
+    {
         dist.x = 0;
         dist.y = 0;
     }
     return dist;
 }
 
-float shortest_to_line(LINE l, COORD p){
+//linha definida por 2 pontos que gera
+float shortest_to_line(LINE l, COORD p)
+{
     // https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
     float n1 = (l.p2.x - l.p1.x) * (l.p1.y - p.y);
     float n2 = (l.p1.x - p.x) * (l.p2.y - l.p1.y);
@@ -66,32 +79,45 @@ float shortest_to_line(LINE l, COORD p){
     return fabs(n1 - n2) / pow(d1 + d2, 0.5);
 }
 
-COORD lc_collision_normal(HITBOX c, LINE l){
+COORD lc_collision_normal(HITBOX c, LINE l)
+{
     COORD normal = {0, 0};
     HITBOX corner;
     corner.r = 0;
     // reta horizontal
-    if(l.p1.y == l.p2.y){
+    if(l.p1.y == l.p2.y)
+    {
         // Se o centro do circulo estiver no meio da reta
-        if(c.center.x > l.p1.x && c.center.x < l.p2.x){
+        if(c.center.x > l.p1.x && c.center.x < l.p2.x)
+        {
             if(shortest_to_line(l, c.center) < c.r) normal = l.normal;
-        } else if(c.center.x <= l.p1.x){
+        }
+        else if(c.center.x <= l.p1.x)
+        {
             corner.center = l.p1;
             normal = cc_collision_normal(corner, c);
-        } else {
+        }
+        else
+        {
             corner.center = l.p2;
             normal = cc_collision_normal(corner, c);
         }
     }
     // reta vertical
-    else{
+    else
+    {
         // Se o centro do circulo estiver no meio da reta
-        if(c.center.y > l.p1.y && c.center.y < l.p2.y){
+        if(c.center.y > l.p1.y && c.center.y < l.p2.y)
+        {
             if(shortest_to_line(l, c.center) < c.r) normal = l.normal;
-        } else if(c.center.y <= l.p1.y){
+        }
+        else if(c.center.y <= l.p1.y)
+        {
             corner.center = l.p1;
             normal = cc_collision_normal(corner, c);
-        } else {
+        }
+        else
+        {
             corner.center = l.p2;
             normal = cc_collision_normal(corner, c);
         }
@@ -99,46 +125,56 @@ COORD lc_collision_normal(HITBOX c, LINE l){
     return normal;
 }
 
-COORD cc_collision_normal(HITBOX c1, HITBOX c2){
+COORD cc_collision_normal(HITBOX c1, HITBOX c2)
+{
     COORD normal;
     normal = dist_from_to(c2.center, c1.center);
-    if(module(normal) > c1.r + c2.r) {
+    if(module(normal) > c1.r + c2.r)
+    {
         normal.x = 0;
         normal.y = 0;
-    } else {
+    }
+    else
+    {
         normalize(&normal);
     }
     return normal;
 }
 
-float dot_prod(COORD v1, COORD v2){
+float dot_prod(COORD v1, COORD v2)
+{
     return v1.x * v2.x + v1.y * v2.y;
 }
 
-COORD multiply(float scalar, COORD v){
+COORD multiply(float scalar, COORD v)
+{
     COORD mult;
     mult.x = v.x * scalar;
     mult.y = v.y * scalar;
     return mult;
 }
 
-void subtract(COORD *v1, COORD v2){
+void subtract(COORD *v1, COORD v2)
+{
     v1->x -= v2.x;
     v1->y -= v2.y;
 }
 
-void vec_sum(COORD *v1, COORD v2){
+void vec_sum(COORD *v1, COORD v2)
+{
     v1->x += v2.x;
     v1->y += v2.y;
 }
 
-COORD project_a_on_b(COORD a, COORD b){
+COORD project_a_on_b(COORD a, COORD b)
+{
     float dot = dot_prod(a, b) / dot_prod(b, b);
     if(dot < 0) dot = 0;
     // Projeção = (a . b) / (b . b) * b
     return multiply(dot, b);
 }
 
-void rm_direction(COORD direction, COORD *vector){
+void rm_direction(COORD direction, COORD *vector)
+{
     subtract(vector, project_a_on_b(*vector, direction));
 }
--- src/main.c
+++ src/main.c
@@ -5,58 +5,81 @@
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_color.h>
 
-#define N_ENV 10
+//al_ :Funcoes prontas do Allegro
 
-int main(){
+int main()
+{
     ALLEGRO_DISPLAY *display;
     ALLEGRO_TIMER *timer;
+    //fila de eventos p serem processados, organiza tudo
     ALLEGRO_EVENT_QUEUE *queue;
     ALLEGRO_EVENT event;
-    ALLEGRO_KEYBOARD_STATE *ret_state;
     SPRITE sprites[SPRITES_MAX];
     ANIMATION anims[ANIMS_MAX];
     CHARACTER characters[3];
-    COORD pos_mage = {320, 240};
-    COORD pos_demon = {0, 0};
-    COORD pos_zombie = {600, 450};
     KEYBOARD_STATE key_pressed;
     ALLEGRO_MONITOR_INFO monitor;
     LINE wall_south;
-    
-    if(!al_init()){
+
+    //intalar as coisas do allegro
+    if(!al_init())
+    {
         return -1;
     }
     al_install_keyboard();
     al_install_mouse();
-    if(al_get_monitor_info(0, &monitor)){
+    if(al_get_monitor_info(0, &monitor))
+    {
         printf("%d, %d", monitor.x2, monitor.y2);
-    } else {
+    }
+    else
+    {
         printf("Não consegui");
     }
     display = al_create_display((int) monitor.x2*0.6, (int) monitor.y2 * 0.6);
-    
-    if (!display) {
-       return -1;
+
+    if (!display)
+    {
+        return -1;
     }
-    timer = al_create_timer(1.0 / FPS);
+
+
+    timer = al_create_timer(1.0 / FPS); //quanto tempo a tela sera atualizada
+    //inicia os eventos
     queue = al_create_event_queue();
+
+    //local de onde vai pegar os eventos
     al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_display_event_source(display));
     al_register_event_source(queue, al_get_timer_event_source(timer));
+
+    //começar a contagem de tempo
     al_start_timer(timer);
+
+    //titulo da tela
     al_set_window_title(display, "O mago");
 
+    //pegar sprites
     al_init_image_addon();
+
+    //p representar o muro vermelho
     al_init_primitives_addon();
 
-    carrega_sprites(sprites, anims, "../img");
+    //coloca todos os sprites e imagens que estão na pasta aos vetores
+    carrega_sprites(sprites, anims, "../img"); //.. :Diretorio de cima
 
-    // Personagem principal
+
+    //Personagem principal
     characters[0].anims = get_anim(anims, "wizzard_m");
-    characters[0].current = characters[0].anims[0];
+    //current é animaçao atual do main
+    characters[0].current = characters[0].anims[0];//ponteiro para as animaçoes do main
+    //velocidade dele
     characters[0].speed = 3;
+    //para detectar colisoes do main
     set_character_hitbox(&characters[0]);
+    //tectar a vida do main
     characters[0].alive = 1;
+    //padronizar o comportamento de um ps
     characters[0].type = MainCharacter;
 
     characters[1].anims = get_anim(anims, "big_demon");
@@ -65,6 +88,7 @@ int main(){
     set_character_hitbox(&characters[1]);
     characters[1].alive = 1;
     characters[1].type = EnemySkeleton;
+    //determinam a posicao do ps, seu centro x y
     characters[1].hitbox.center.x = 600;
     characters[1].hitbox.center.y = 400;
 
@@ -77,27 +101,42 @@ int main(){
     characters[2].hitbox.center.x = 600;
     characters[2].hitbox.center.y = 0;
 
-
+    //add retas para colisao
     // muro do sul
     wall_south.normal.y = 1;
     wall_south.normal.x = 0;
+    //cordenadas
     wall_south.p1.x = 0;
     wall_south.p2.x = 600;
     wall_south.p1.y = 400;
     wall_south.p2.y = 400;
 
-    do{
-        al_wait_for_event(queue, &event);
-        if(event.type == ALLEGRO_EVENT_TIMER){
+    do
+    {
+        //espera ocorrer algo na fila de evento para executar
+        al_wait_for_event(queue, &event);//passando o endereço, vai modificando a cada atualização
+
+        if(event.type == ALLEGRO_EVENT_TIMER)
+        {
+            //limpa a tela p preto (tabela RGB 000) p cada atualização
             al_clear_to_color(al_map_rgb_f(0, 0, 0));
+
+            //atualização da bio p cada ps
             update_character(&characters[0], key_pressed, characters, wall_south);
             update_character(&characters[1], key_pressed, characters, wall_south);
             update_character(&characters[2], key_pressed, characters, wall_south);
+
+            //desenha a linha p allegro
             al_draw_line(0, 400, 600, 400, al_color_name("red"), 1);
+
+            //atualiza tela (?)
             al_flip_display();
         }
-        set_kb_state(&key_pressed, event);
-    }while(event.type != ALLEGRO_EVENT_DISPLAY_CLOSE);
+        //saber quais teclas estão sendo apertadas
+        set_kb_state(&key_pressed, event);//&key referencia de quais teclas estão sendo usadas
+    }
+    //A allegro roda ate tu apertar 'X' p encerrar o prog
+    while(event.type != ALLEGRO_EVENT_DISPLAY_CLOSE);
 
     return 0;
 }
